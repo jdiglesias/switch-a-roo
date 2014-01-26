711125c3a1fe51e325af36fd64a85c09e93a9e43
Juan Iglesias <juaniglesias@juans-air.evergreen.edu>
2014-01-26 14:55:13 -0800

    default introjucer files

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,188 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+SwitcharooAudioProcessor::SwitcharooAudioProcessor()
+{
+}
+
+SwitcharooAudioProcessor::~SwitcharooAudioProcessor()
+{
+}
+
+//==============================================================================
+const String SwitcharooAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+int SwitcharooAudioProcessor::getNumParameters()
+{
+    return 0;
+}
+
+float SwitcharooAudioProcessor::getParameter (int index)
+{
+    return 0.0f;
+}
+
+void SwitcharooAudioProcessor::setParameter (int index, float newValue)
+{
+}
+
+const String SwitcharooAudioProcessor::getParameterName (int index)
+{
+    return String::empty;
+}
+
+const String SwitcharooAudioProcessor::getParameterText (int index)
+{
+    return String::empty;
+}
+
+const String SwitcharooAudioProcessor::getInputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+const String SwitcharooAudioProcessor::getOutputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+bool SwitcharooAudioProcessor::isInputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool SwitcharooAudioProcessor::isOutputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool SwitcharooAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SwitcharooAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SwitcharooAudioProcessor::silenceInProducesSilenceOut() const
+{
+    return false;
+}
+
+double SwitcharooAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SwitcharooAudioProcessor::getNumPrograms()
+{
+    return 0;
+}
+
+int SwitcharooAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SwitcharooAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String SwitcharooAudioProcessor::getProgramName (int index)
+{
+    return String::empty;
+}
+
+void SwitcharooAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void SwitcharooAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void SwitcharooAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+void SwitcharooAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    for (int channel = 0; channel < getNumInputChannels(); ++channel)
+    {
+        float* channelData = buffer.getSampleData (channel);
+
+        // ..do something to the data...
+    }
+
+    // In case we have more outputs than inputs, we'll clear any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    for (int i = getNumInputChannels(); i < getNumOutputChannels(); ++i)
+    {
+        buffer.clear (i, 0, buffer.getNumSamples());
+    }
+}
+
+//==============================================================================
+bool SwitcharooAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* SwitcharooAudioProcessor::createEditor()
+{
+    return new SwitcharooAudioProcessorEditor (this);
+}
+
+//==============================================================================
+void SwitcharooAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SwitcharooAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SwitcharooAudioProcessor();
+}
