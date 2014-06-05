7842dac30b82afc1ebe3cb7dfccf274508b0350c
yougab <younggabe9@gmail.com>
2014-06-04 23:56:10 -0700

    added juce library code

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,62 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#ifndef BINARYDATA_H_65356118_INCLUDED
+#define BINARYDATA_H_65356118_INCLUDED
+
+namespace BinaryData
+{
+    extern const char*   CHANGELOG;
+    const int            CHANGELOGSize = 5070;
+
+    extern const char*   COPYING;
+    const int            COPYINGSize = 1475;
+
+    extern const char*   Makefile;
+    const int            MakefileSize = 1437;
+
+    extern const char*   README;
+    const int            READMESize = 5627;
+
+    extern const char*   README_simd;
+    const int            README_simdSize = 2419;
+
+    extern const char*   compfft_py;
+    const int            compfft_pySize = 2187;
+
+    extern const char*   fastfir_py;
+    const int            fastfir_pySize = 2614;
+
+    extern const char*   fft_py;
+    const int            fft_pySize = 4794;
+
+    extern const char*   Makefile2;
+    const int            Makefile2Size = 2692;
+
+    extern const char*   mk_test_py;
+    const int            mk_test_pySize = 3009;
+
+    extern const char*   testkiss_py;
+    const int            testkiss_pySize = 3575;
+
+    extern const char*   TIPS;
+    const int            TIPSSize = 2126;
+
+    extern const char*   Makefile3;
+    const int            Makefile3Size = 1678;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Number of elements in the namedResourceList array.
+    const int namedResourceListSize = 13;
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
+}
+
+#endif
